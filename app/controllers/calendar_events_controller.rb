56b7736410834191186ad24a0feff5f36b8ad872
Joshua Woodward <jdw6359@rit.edu>
2016-02-19 12:13:36 -0500

    Implement calendar_event and stock_transaction controller

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -13,12 +13,19 @@ class CalendarEventsController < ApplicationController
 		render json: @event
 	end
 
-#	def create
-#		@current_user.create_calendar_event(calendar_event_params)
-#	end
-#
-#	private
-#	def calendar_event_params
-#		params.require(:calendar_event).permit(:name, :location, :start_time, :end_time)
-#	end
+	api :POST, '/calendar_events', 'Create a new calendar event'
+	param :calendar_event, Hash, "Calendar object to create" do
+		param :name, String, 'Name of the Calendar Event'
+		param :location, String, 'Location of the Calendar Event'
+		param :start_time, DateTime, 'Start Time of the Event'
+		param :end_time, DateTime, 'End Time of the Event'
+	end 
+	def create
+		@current_user.create_calendar_event(calendar_event_params)
+	end
+
+	private
+	def calendar_event_params
+		params.require(:calendar_event).permit(:name, :location, :start_time, :end_time)
+	end
 end
