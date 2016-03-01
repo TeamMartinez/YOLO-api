d0d5a50d6d2c57362811c41a8eb78594dbc5e666
Joshua Woodward <jdw6359@rit.edu>
2016-02-29 19:22:46 -0500

    Rendering 403 response to client if current user not set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,12 @@ class ApplicationController < ActionController::Base
 		if ENV["RAILS_ENV"].eql?("development")
 			@current_user ||= User.find 1
 		else
+			# attempt to set current user
 			@current_user ||= User.find(session[:user_id]) if session[:user_id]
+
+			# if the current user was not set, render unauthorized response to client
+			render json: {errors: "Current user was not able to be assigned"},
+				status: 403 unless @current_user
 		end
 	end
 end
