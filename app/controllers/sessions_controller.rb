4ad1169e5c663664480ef8afc2f7449410e3454b
Kocsen <kocsenc@gmail.com>
2016-03-03 13:55:42 -0500

    redirects to appropriate url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]     
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to "http://vm344a.se.rit.edu", :notice => "Signed in!"
+    redirect_to "http://vm344a.se.rit.edu/", :notice => "Signed in!"
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to "http://vm344a.se.rit.edu", :notice => "Signed out!"
+    redirect_to "http://vm344a.se.rit.edu/", :notice => "Signed out!"
   end
 end
