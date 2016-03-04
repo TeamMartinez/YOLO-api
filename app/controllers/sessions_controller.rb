6c5a7325165f3a253ecd65f4ec9f282bf0526f26
Joshua Woodward <jdw6359@rit.edu>
2016-03-04 16:27:23 -0500

    Perform session redirect from environment variable

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,13 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]     
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to "http://vm344a.se.rit.edu/", :notice => "Signed in!"
+    redirect_to ENV["YOLO_SESSION_REDIRECT_URL"], :notice => "Signed in!"
   end
 
   # destroy a session
   def destroy
     session[:user_id] = nil
-    redirect_to "http://vm344a.se.rit.edu/", :notice => "Signed out!"
+    redirect_to ENV["YOLO_SESSION_REDIRECT_URL"], :notice => "Signed out!"
   end
 
   # verify the user is authenticated
