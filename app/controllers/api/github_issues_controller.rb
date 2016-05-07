35a91232edf14dc0a211c06e69cd5d0e95db8e9d
Joshua Woodward <jdw6359@rit.edu>
2016-05-07 18:31:31 -0400

    refactor github issues

--- app/controllers/api/github_issues_controller.rb
+++ app/controllers/api/github_issues_controller.rb
@@ -3,22 +3,22 @@ class Api::GithubIssuesController < ApplicationController
   before_action :create_github_client, only: [:index, :create]
 
   def index
-    @issues = @client.issues.list user: 'TeamMartinez', repo: 'YOLO-api', state: 'open'
-    render json: @issues
+    render json: client_issues
   end
 
   def create
     # TODO: make user: and repo: params default somewhere and use params
-    @client.issues.create user: 'TeamMartinez', repo: 'YOLO-api', title: params[:title]
-    render json: { errors: "not implemented" }
+    if @client.issues.create user: 'TeamMartinez', repo: 'YOLO-api', title: params[:title]
+      render json: client_issues
+    end
   end
 
   private
-  def create_github_client
-    @client = Github.new oauth_token: ENV['YOLO_ISSUES_TOKEN']
+  def client_issues
+    @client.issues.list user: 'TeamMartinez', repo: 'YOLO-api', state: 'open'
   end
 
-  def issues_params
-    params
+  def create_github_client
+    @client = Github.new oauth_token: ENV['YOLO_ISSUES_TOKEN']
   end
 end
