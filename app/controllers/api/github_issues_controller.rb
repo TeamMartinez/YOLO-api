855b332bbf5fd7fefce9a172ff7ee81f83577204
Kocsen <kocsenc@gmail.com>
2016-05-06 22:20:39 -0400

    Implement GH issue create

--- app/controllers/api/github_issues_controller.rb
+++ app/controllers/api/github_issues_controller.rb
@@ -3,11 +3,13 @@ class Api::GithubIssuesController < ApplicationController
   before_action :create_github_client, only: [:index, :create]
 
   def index
-    @issues = @client.issues.list
+    @issues = @client.issues.list user: 'TeamMartinez', repo: 'YOLO-api', state: 'open'
     render json: @issues
   end
 
   def create
+    # TODO: make user: and repo: params default somewhere and use params
+    @client.issues.create user: 'TeamMartinez', repo: 'YOLO-api', title: params[:title]
     render json: { errors: "not implemented" }
   end
 
